ae6d63a4cf253f221fc460bfa83623f6dc022618
Laika-Beats <laikabeatsdenver@gmail.com>
2021-03-24 14:50:14 -0600

    db c+ localhost connected

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,26 @@
-require("dontenv").config();
+// Dependencies
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+
+// Express
+app.use(cors());
+app.use(express.json());
+
+// DB
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("🌐🌐🌐 Connected to Database."));
+
+// Connect to localhost
+app.listen(process.env.PORT, () =>
+  console.log(
+    "🌎🌎🌎 Server Started. Listening on http://localhost:" + process.env.PORT
+  )
+);
