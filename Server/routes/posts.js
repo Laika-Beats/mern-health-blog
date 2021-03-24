7ed1f436032deeceda666fbe5d273a11366dae10
Laika-Beats <laikabeatsdenver@gmail.com>
2021-03-24 15:04:39 -0600

    GET ALL and CREATE Post routes

---
+++ Server/routes/posts.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const Post = require("../models/post.js");
+
+// GET ALL Posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// CREATE Post
+router.post("/", async (req, res) => {
+  const post = new Post({
+    message: req.body.message,
+    category: req.body.category,
+  });
+  try {
+    const newPost = await post.save();
+    res.status(201).json({ newPost });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
