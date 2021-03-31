8d7043d031f2898f18146969b9dc587bf2cd8fce
Laika-Beats <laikabeatsdenver@gmail.com>
2021-03-30 22:12:11 -0600

    blog entry front-end

--- Client/src/components/CreateEntry.js
+++ Client/src/components/CreateEntry.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const CreatEntry = () => {
+  const [blogEntry, setBlogEntry] = useState("");
+  const [blogEntries, setBlogEntries] = useState([]);
+
+  const inputText = (e) => {
+    setBlogEntry(e.target.value);
+    console.log(blogEntry);
+  };
+
+  const blogEntryHandler = (e) => {
+    e.preventDefault();
+    setBlogEntries([...blogEntries, blogEntry]);
+  };
+
   return (
-    <form>
-      <textarea></textarea>
+    <form onSubmit={blogEntryHandler}>
+      <textarea onChange={inputText}></textarea>
       <button>Submit</button>
     </form>
   );
